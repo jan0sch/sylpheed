17191434b7ed1972467b2faf45a210801c2850e6
Hiroyuki Yamamoto <hiro-y@kcn.ne.jp>
2005-07-22 07:08:31 +0000

    tagged 2.0.0beta6.

--- src/compose.c
+++ src/compose.c
@@ -2062,13 +2062,13 @@ static gboolean compose_get_line_break_pos(GtkTextBuffer *buffer,
 	gint pos = 0;
 	gboolean can_break = FALSE;
 	gboolean do_break = FALSE;
-	gboolean prev_hyphen = FALSE;
+	gboolean prev_dont_break = FALSE;
 
 	gtk_text_iter_forward_to_line_end(&line_end);
 	str = gtk_text_buffer_get_text(buffer, &iter, &line_end, FALSE);
 	len = g_utf8_strlen(str, -1);
-	//g_print("breaking line: %d: %s (len = %d)\n",
-	//	gtk_text_iter_get_line(&iter), str, len);
+	/* g_print("breaking line: %d: %s (len = %d)\n",
+		gtk_text_iter_get_line(&iter), str, len); */
 	attrs = g_new(PangoLogAttr, len + 1);
 
 	pango_default_break(str, -1, NULL, attrs, len + 1);
@@ -2096,7 +2096,7 @@ static gboolean compose_get_line_break_pos(GtkTextBuffer *buffer,
 		gunichar wc;
 		gint uri_len;
 
-		if (attr->is_line_break && can_break && !prev_hyphen)
+		if (attr->is_line_break && can_break && !prev_dont_break)
 			pos = i;
 
 		/* don't wrap URI */
@@ -2115,7 +2115,7 @@ static gboolean compose_get_line_break_pos(GtkTextBuffer *buffer,
 		wc = g_utf8_get_char(p);
 		if (g_unichar_iswide(wc)) {
 			col += 2;
-			if (prev_hyphen && can_break && attr->is_line_break)
+			if (prev_dont_break && can_break && attr->is_line_break)
 				pos = i;
 		} else if (*p == '\t')
 			col += 8;
@@ -2126,10 +2126,10 @@ static gboolean compose_get_line_break_pos(GtkTextBuffer *buffer,
 			break;
 		}
 
-		if (*p == '-')
-			prev_hyphen = TRUE;
+		if (*p == '-' || *p == '/')
+			prev_dont_break = TRUE;
 		else
-			prev_hyphen = FALSE;
+			prev_dont_break = FALSE;
 
 		p = g_utf8_next_char(p);
 		can_break = TRUE;
@@ -6239,6 +6239,9 @@ static void compose_attach_drag_received_cb (GtkWidget		*widget,
 	if (info == DRAG_TYPE_RFC822)
 		content_type = "message/rfc822";
 
+	debug_print("compose_attach_drag_received_cb(): received %s\n",
+		    (const gchar *)data->data);
+
 	list = uri_list_extract_filenames((const gchar *)data->data);
 	for (cur = list; cur != NULL; cur = cur->next) {
 		path = (gchar *)cur->data;
@@ -6249,6 +6252,10 @@ static void compose_attach_drag_received_cb (GtkWidget		*widget,
 	}
 	if (list) compose_changed_cb(NULL, compose);
 	g_list_free(list);
+
+	if ((drag_context->actions & GDK_ACTION_MOVE) != 0)
+		drag_context->action = 0;
+	gtk_drag_finish(drag_context, TRUE, FALSE, time);
 }
 
 static void compose_insert_drag_received_cb (GtkWidget		*widget,
@@ -6262,12 +6269,40 @@ static void compose_insert_drag_received_cb (GtkWidget		*widget,
 {
 	Compose *compose = (Compose *)user_data;
 	GList *list, *cur;
+	static GdkDragContext *context_ = NULL;
+	static gint x_ = -1, y_ = -1;
+	static guint info_ = N_DRAG_TYPES;
+	static guint time_ = G_MAXUINT;
+
+	debug_print("compose_insert_drag_received_cb(): received %s\n",
+		    (const gchar *)data->data);
+
+	/* FIXME: somehow drag-data-received signal is emitted twice.
+	 * This hack prevents duplicated insertion. */
+	if (context_ == drag_context && x_ == x && y_ == y && info_ == info &&
+	    time_ == time) {
+		debug_print("dup event\n");
+		context_ = NULL;
+		x_ = y_ = -1;
+		info_ = N_DRAG_TYPES;
+		time_ = G_MAXUINT;
+		return;
+	}
+	context_ = drag_context;
+	x_ = x;
+	y_ = y;
+	info_ = info;
+	time_ = time;
 
 	list = uri_list_extract_filenames((const gchar *)data->data);
 	for (cur = list; cur != NULL; cur = cur->next)
 		compose_insert_file(compose, (const gchar *)cur->data, TRUE);
 	list_free_strings(list);
 	g_list_free(list);
+
+	if ((drag_context->actions & GDK_ACTION_MOVE) != 0)
+		drag_context->action = 0;
+	gtk_drag_finish(drag_context, TRUE, FALSE, time);
 }
 
 static void to_activated(GtkWidget *widget, Compose *compose)
